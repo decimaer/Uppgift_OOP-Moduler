4d0a4cee6295b15fc4c6461cd0eee85675c8ab75
Hampus Dufberg Töttrup <Hampus>
2023-01-11 16:13:14 +0100

    added rendering of player hands

--- js/poker.js
+++ js/poker.js
@@ -110,7 +110,9 @@ class Game {
 	constructor() {}
 
 	startGame = function () {
-		console.log("hej");
+		if (this.players.length < 2) return;
+
+		//todo: check if there are cards left
 		this.players.forEach(
 			(player) => (player.hand = this.dealer.dealCards(5))
 		);
@@ -125,7 +127,6 @@ class Game {
 	};
 
 	addPlayer = function (playerName) {
-		//todo: validate player name
 		this.players.push(new Player(playerName));
 	};
 }
@@ -133,13 +134,19 @@ class Game {
 const game = new Game();
 
 const controlAddNewPlayer = function (playerName) {
-	console.log(playerName);
 	game.addPlayer(playerName);
 
 	pokerView.renderGame(game.players);
 };
 
+const controlStartGame = function () {
+	game.startGame();
+
+	pokerView.renderGame(game.players);
+};
+
 pokerView.addHandlerRender(controlAddNewPlayer);
+pokerView.addHandlerStartGame(controlStartGame);
 
 // game.addPlayer("Slim");
 // game.addPlayer("Luke");
@@ -150,3 +157,9 @@ console.log(game.players);
 /* let slim = dealer.dealCards(5)
 let luke = dealer.dealCards(5)
 console.log(Validation.check([slim, luke])) */
+
+const devInit = function () {
+	controlAddNewPlayer("Slim");
+	controlAddNewPlayer("Luke");
+};
+devInit();
--- js/pokerView.js
+++ js/pokerView.js
@@ -1,16 +1,51 @@
 class PokerView {
-	btnAddPlayer = document.getElementById("btnAddPlayer");
 	inputAddPlayer = document.getElementById("inputAddPlayer");
+	btnAddPlayer = document.getElementById("btnAddPlayer");
+	btnStartGame = document.getElementById("btnStartGame");
 	outputPokerGame = document.getElementById("outputPokerGame");
 	outputNodes = document.getElementById("outputPokerGame").childNodes;
 
+	markupPlayerRow() {
+		return;
+	}
+	/**
+	 * Renders the game area in the DOM
+	 * @param {Array} playersArray the array of player objects from the game object
+	 */
+
 	renderGame(playersArray) {
-		const markup = playersArray
+		let markup = "";
+
+		playersArray.forEach((player) => {
+			console.log(player.name.padEnd(20));
+			markup += `
+				<div class="playerRow" id="${player.name}">
+				<span>${player.name.padEnd(15)}</span>
+				<span class="cardHand">${
+					player.hand
+						? player.hand
+								.map((card) => {
+									const className =
+										card.suit === "♦" || card.suit === "♥"
+											? "red"
+											: "black";
+									return `<span class="${className}">${card.cardIcon}</span>`;
+								})
+								.join("")
+								.padEnd(10)
+						: "🂠🂠🂠🂠🂠"
+				}</span>
+				</div>
+			`;
+		});
+		this.outputPokerGame.innerHTML = markup;
+
+		/* 		const markup = playersArray
 			.map((player) => `<p>${player.name}</p>`)
 			.join("");
 		this.outputPokerGame.innerHTML = markup;
+	 */
 	}
-
 	addHandlerRender(handler) {
 		btnAddPlayer.addEventListener("click", function (e) {
 			e.preventDefault();
@@ -18,12 +53,18 @@ class PokerView {
 			const playerName = inputAddPlayer.value;
 			if (!playerName) return;
 
-			handler(playerName);
+			const validatedName = playerName.trim().slice(0, 10);
+
+			handler(validatedName);
 
 			inputAddPlayer.value = "";
 
-			console.log(`New player \"${playerName}\" added!`);
+			console.log(`New player \"${validatedName}\" added!`);
 		});
+	}
+
+	addHandlerStartGame(handler) {
+		this.btnStartGame.addEventListener("click", handler);
 	}
 }
 
